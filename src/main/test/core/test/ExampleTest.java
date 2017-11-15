79ad71c97e16eb7f2001ee4432bc8afce359e287
code-io <mmsayani@ucdavis.edu>
2017-11-15 10:15:26 -0800

    tests added

--- src/main/test/core/test/ExampleTest.java
+++ src/main/test/core/test/ExampleTest.java
@@ -1,33 +1,37 @@
 package core.test;
 
-import core.api.IAdmin;
-import core.api.impl.Admin;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import core.api.IAdmin;
+import core.api.IInstructor;
+import core.api.IStudent;
+import core.api.impl.Admin;
+import core.api.impl.Student;
+import core.api.impl.Instructor;
 
-/**
- * Created by Vincent on 23/2/2017.
- */
 public class ExampleTest {
 
     private IAdmin admin;
-
+    
     @Before
     public void setup() {
         this.admin = new Admin();
     }
-
-    @Test
+    
+    @Test // Creating a valid class (should succeed) 
     public void testMakeClass() {
         this.admin.createClass("Test", 2017, "Instructor", 15);
         assertTrue(this.admin.classExists("Test", 2017));
     }
 
-    @Test
+    @Test // Creating a class in the past (expect fail)
     public void testMakeClass2() {
         this.admin.createClass("Test", 2016, "Instructor", 15);
         assertFalse(this.admin.classExists("Test", 2016));
-    }
+    }  
 }
